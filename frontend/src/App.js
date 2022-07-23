011c607877d97d9bb49eccb4527159b9cfbe0902
Pham-Van-Khiem-Toan-Tin <81103797+Pham-Van-Khiem-Toan-Tin@users.noreply.github.com>
2022-07-24 05:19:24 +0700

    Khiem

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,29 @@
-import Form from './components/form';
 import 'bootstrap';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './v1/components/layouts/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './v1/components/Home/Home';
+import Categories from './v1/components/categories/Categories';
+import Contact from './v1/components/Contact/Contact';
+import Cart from './v1/components/Cart/Cart';
+import User from './v1/components/User/User';
+import About from './v1/components/About/About';
+import Admin from './v1/components/Admin/Admin';
 function App() {
   return (
-    <div className="App">
-      <Form />
-    </div>
+    <>
+     <Header />
+     <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/categories" element={<Categories />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/user" element={<User />} />
+      <Route path="/about" element={<About />} />
+      <Route path='/admin' element={<Admin />} />
+     </Routes> 
+    </>
   );
 }
 
