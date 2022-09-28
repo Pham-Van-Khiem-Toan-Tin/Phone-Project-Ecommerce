a5eb3eadf44c5bea29144c05eab0df74527afd8c
Pham-Van-Khiem-Toan-Tin <81103797+Pham-Van-Khiem-Toan-Tin@users.noreply.github.com>
2022-09-29 01:50:22 +0700

    Khiem

--- frontend/src/v1/components/layouts/Footer.jsx
+++ frontend/src/v1/components/layouts/Footer.jsx
@@ -1,38 +1,38 @@
 import React from "react";
-
+import "./Footer.css";
 const Footer = () => {
   return (
     <>
-      <div className="container">
+      <div className="Footer">
         <footer className="py-3 my-4">
           <ul className="nav justify-content-center border-bottom pb-3 mb-3">
             <li className="nav-item">
-              <div  className="nav-link px-2 text-muted">
+              <div  className="nav-link px-2">
                 Home
               </div>
             </li>
             <li className="nav-item">
-              <div  className="nav-link px-2 text-muted">
+              <div  className="nav-link px-2">
                 Features
               </div>
             </li>
             <li className="nav-item">
-              <div  className="nav-link px-2 text-muted">
+              <div  className="nav-link px-2">
                 Pricing
               </div>
             </li>
             <li className="nav-item">
-              <div  className="nav-link px-2 text-muted">
+              <div  className="nav-link px-2">
                 FAQs
               </div>
             </li>
             <li className="nav-item">
-              <div  className="nav-link px-2 text-muted">
+              <div  className="nav-link px-2">
                 About
               </div>
             </li>
           </ul>
-          <p className="text-center text-muted">© 2022 Company, Inc</p>
+          <p className="text-center">© 2022 Company, Inc</p>
         </footer>
       </div>
     </>
