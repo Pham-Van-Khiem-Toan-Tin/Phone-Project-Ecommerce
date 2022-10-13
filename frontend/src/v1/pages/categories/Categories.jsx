979c6246b6092adc43656a4d27219322f2ee5ecb
Pham-Van-Khiem-Toan-Tin <81103797+Pham-Van-Khiem-Toan-Tin@users.noreply.github.com>
2022-10-14 05:57:11 +0700

    Khiem

--- frontend/src/v1/pages/categories/Categories.jsx
+++ frontend/src/v1/pages/categories/Categories.jsx
@@ -1,47 +1,17 @@
-import React, { useState } from "react";
-import { Range } from 'react-range';
+import React, { useRef, useState } from "react";
+import RangeSlider from 'react-range-slider-input';
 import { Data2 } from "../../../data/Data";
 import "./Categories.css";
+
 const Categories = () => {
-  const [rangeValues, setRangeValues] = useState(50)
+  
   return (
     <div className="categories">
       <div className="products-controller">
-      <Range
-        step={0.1}
-        min={0}
-        max={100}
-        values={rangeValues}
-        onChange={(e) => setRangeValues(e.target.values)}
-        renderTrack={({ props, children }) => (
-          <div
-            {...props}
-            style={{
-              ...props.style,
-              height: '6px',
-              width: '100%',
-              backgroundColor: '#ccc'
-            }}
-          >
-            {children}
-          </div>
-        )}
-        renderThumb={({ props }) => (
-          <div
-            {...props}
-            style={{
-              ...props.style,
-              height: '42px',
-              width: '42px',
-              backgroundColor: '#999'
-            }}
-          />
-        )}
-      />
+      THis is controller
+        <RangeSlider min={0} max={1000} step={100} defaultValue={[0,1000]}/>
       </div>
-      <div className="product-gird">
-        This is grid
-      </div>
+      <div className="product-gird">This is grid</div>
     </div>
   );
 };
