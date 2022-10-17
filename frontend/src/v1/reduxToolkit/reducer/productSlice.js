6d9e1284eb7254003c3284aea1cabefcb0481788
Pham-Van-Khiem-Toan-Tin <81103797+Pham-Van-Khiem-Toan-Tin@users.noreply.github.com>
2022-10-17 14:45:02 +0700

    Khiem

--- frontend/src/v1/reduxToolkit/reducer/productSlice.js
+++ frontend/src/v1/reduxToolkit/reducer/productSlice.js
@@ -1,3 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { getAdminProducts} from "../actions/productAction"
+import { getAdminProducts} from "../actions/productAction";
+
+const productSlice = createSlice({
+    name: "product",
+    initialState: {
+        
+    }
+})
 
