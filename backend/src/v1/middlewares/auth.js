7e5ecbc814c5fa6f782477eeff70a0b66b2878e9
Pham-Van-Khiem-Toan-Tin <phamkhiemhust2001@gmail.com>
2023-04-10 05:37:46 +0700

    Khiem

--- backend/src/v1/middlewares/auth.js
+++ backend/src/v1/middlewares/auth.js
@@ -4,15 +4,9 @@ const ErrorHandle = require("../utils/errorHandle");
 require("dotenv").config();
 
 module.exports.isAuthenticatedUser = async (req, res, next) => {
-<<<<<<< HEAD
   const { accessToken } = req.body;
   console.log(req.body);
   if (!accessToken) {
-=======
-  const {refeshToken} = req.cookies;
-  console.log(req.cookies);
-  if(!refeshToken) {
->>>>>>> c17094c894dc9b4745fe69861ed2f6cd1bfe3025
     return next(new ErrorHandle("Please login to access this resource"));
   } else {
     const decodeData = await jwt.verify(
@@ -29,12 +23,6 @@ module.exports.isAuthenticatedUser = async (req, res, next) => {
       });
     }
   }
-<<<<<<< HEAD
-=======
-  const decodeData = jwt.verify(refeshToken, process.env.REFESHTOKEN_SECRET);
-  req.user = await userModel.findById(decodeData.id);
-  next();
->>>>>>> c17094c894dc9b4745fe69861ed2f6cd1bfe3025
 };
 
 module.exports.isAuthorizeRoles = (...roles) => {
@@ -51,10 +39,7 @@ module.exports.isAuthorizeRoles = (...roles) => {
     next();
   };
 };
-<<<<<<< HEAD
 
 module.exports.generateToken = async (data, secret, expires) => {
   return await jwt.sign(data, secret, { expiresIn: expires });
 };
-=======
->>>>>>> c17094c894dc9b4745fe69861ed2f6cd1bfe3025
