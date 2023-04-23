87fbde4bcf6668cea4052ec525eb8f1f6c068de4
Pham-Van-Khiem-Toan-Tin <81103797+Pham-Van-Khiem-Toan-Tin@users.noreply.github.com>
2023-04-23 07:08:14 +0700

    Khiem

--- frontend/src/v1/pages/OrderConfirm/OrderConFirm.jsx
+++ frontend/src/v1/pages/OrderConfirm/OrderConFirm.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import "./OrderConfirm.css";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const OrderConFirm = () => {
+  const navigate = useNavigate();
+  const {user} = useSelector((state) => state.user);
+  const {shippingInFor, cartList} = useSelector((state) => state.cart);
+  const address = `${shippingInFor.address}, ${shippingInFor.city}, ${shippingInFor.pinCode}, ${shippingInFor.country}`;
+  useEffect(() => {
+    if(!user) {
+      navigate("../../login");
+    } 
+  }, [])
+  
   return (
-    <div>OrderConFirm</div>
-  )
-}
+    <div className="orderconfirm">
+      <div className="shippingInfor">
+        <div className="shippingInfor-content">
+          <h4>Shipping Infor:</h4>
+          <div className="userInfor">
+            <div>Name: {user?.name}</div>
+            <div>Phone: {shippingInFor?.phoneNum}</div>
+            <div>Address: {address}</div>
+          </div>
+          <h4>Your Cart Items</h4>
+          <div className="cart-list">
+            
+          </div>
+        </div>
+        <div class="vr"></div>
+      </div>
+      <div className="total">
+        <h4>Total</h4>
+      </div>
+    </div>
+  );
+};
 
-export default OrderConFirm
+export default OrderConFirm;
