14f08e5f1a7b3b45e6ad5e8525a2bef8267940d2
Pham-Van-Khiem-Toan-Tin <81103797+Pham-Van-Khiem-Toan-Tin@users.noreply.github.com>
2023-05-11 17:05:59 +0700

    update input edit profile

--- frontend/src/v1/pages/User/UpdateProfile.jsx
+++ frontend/src/v1/pages/User/UpdateProfile.jsx
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./updateProfile.css";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 const UpdateProfile = () => {
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.user);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [imagePre, setImagePre] = useState("");
+  const [first, setfirst] = useState("");
+  useEffect(() => {
+    if(user) {
+      setName(user.name);
+      setEmail(user.email);
+      setImagePre(user.avatar.url)
+    }
+  }, [user]);
   return (
     <div className="edit_profile">
       <form>
         <h5>update profile</h5>
-        <input className="edit_profile_input" type="" />
-        <input className="edit_profile_input" type="" />
+        <input className="edit_profile_input" value={name} type="text" placeholder="Name..." />
+        <input className="edit_profile_input" value={email} type="email" placeholder="Email..." />
         <div className="edit_profile_image">
-          <input className="edit_profile_input" type="file" />
+          <img alt="" />
+          <label htmlFor="edit_profile_image">Chose image</label>
+          <input id="edit_profile_image" type="file" />
         </div>
-        <input type="submit" placeholder="Submit" />
+        <input className="edit_profile_submit" type="submit" value="Submit" />
       </form>
     </div>
   );
