7218eaf8e6371839b37eb73fd94321e947defe23
Pham-Van-Khiem-Toan-Tin <81103797+Pham-Van-Khiem-Toan-Tin@users.noreply.github.com>
2023-05-11 16:41:26 +0700

    update profile

--- frontend/src/v1/pages/User/UpdateProfile.jsx
+++ frontend/src/v1/pages/User/UpdateProfile.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
+import React from "react";
 import "./updateProfile.css";
-import { useDispatch, useSelector } from 'react-redux';
-import { toast } from 'react-toastify';
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
 const UpdateProfile = () => {
-    const dispatch = useDispatch();
-    const {user} = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.user);
   return (
-    <div className='edit_profile'>
-        <form>
-            <h5>update profile</h5>
-            <input className='edit_profile_input' type='' />
-            <input className='edit_profile_input' type='' />
-            <input className='edit_profile_input' type='' />
-        </form>
+    <div className="edit_profile">
+      <form>
+        <h5>update profile</h5>
+        <input className="edit_profile_input" type="" />
+        <input className="edit_profile_input" type="" />
+        <div className="edit_profile_image">
+          <input className="edit_profile_input" type="file" />
+        </div>
+        <input type="submit" placeholder="Submit" />
+      </form>
     </div>
-  )
-}
+  );
+};
 
 export default UpdateProfile;
