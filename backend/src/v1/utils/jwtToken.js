3871e0450a245ec6896f5348f53fcd6dacf32def
Pham-Van-Khiem-Toan-Tin <phamkhiemhust2001@gmail.com>
2023-05-16 03:00:16 +0700

    Update jwtToken.js

--- backend/src/v1/utils/jwtToken.js
+++ backend/src/v1/utils/jwtToken.js
@@ -8,6 +8,7 @@ const sendToken = async (user, status, res) => {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
+    secure: true,
     httpOnly: true,
     domain: '.phone-app-huster.netlify.app'
   };
