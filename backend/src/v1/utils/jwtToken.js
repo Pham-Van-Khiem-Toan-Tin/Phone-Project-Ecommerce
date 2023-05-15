61b58ecdfdf09c89c7ea1d742f1b281f6030351f
Pham-Van-Khiem-Toan-Tin <phamkhiemhust2001@gmail.com>
2023-05-16 03:27:42 +0700

    update cookie

--- backend/src/v1/utils/jwtToken.js
+++ backend/src/v1/utils/jwtToken.js
@@ -8,10 +8,10 @@ const sendToken = async (user, status, res) => {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
-    secure: true,
+    // secure: true,
     // httpOnly: true,
     // sameSite: 'Strict',
-    domain: 'phone-app-huster.netlify.app',
+    domain: '.phone-app-huster.netlify.app',
   };
   res
     .status(status)
