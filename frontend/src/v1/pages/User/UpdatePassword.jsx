8bb4e343374fa0b94e1b74be9be0cc9dd439be48
Pham-Van-Khiem-Toan-Tin <81103797+Pham-Van-Khiem-Toan-Tin@users.noreply.github.com>
2023-12-12 13:16:31 +0700

    change cart ui

--- frontend/src/v1/pages/User/UpdatePassword.jsx
+++ frontend/src/v1/pages/User/UpdatePassword.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./updatePassword.css";
+import "./update-password.css";
 import InputPass from "../../components/InputPass/InputPass";
 import { useDispatch, useSelector } from "react-redux";
 import { updatePassword } from "../../reduxToolkit/actions/userAction";
@@ -10,6 +10,7 @@ import {
 } from "../../reduxToolkit/reducer/user/userHandle";
 import { useNavigate } from "react-router-dom";
 import Loader from "../../components/Loader/Loader";
+import { FaKey } from "react-icons/fa";
 const UpdatePassword = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -53,32 +54,45 @@ const UpdatePassword = () => {
       {isLoading ? (
         <Loader />
       ) : (
-        <div className="edit_password">
-          <form onSubmit={updatePasswordMe}>
-            <h5>Change Password</h5>
-            <div className="box-input">
-              <InputPass
-                value={oldPassword}
-                onChange={changeOldPass}
-                placeholder="Old password..."
-              />
+        <div className="update-password">
+          <form
+            className="form-update rounded d-flex flex-column align-items-center justify-content-center"
+            onSubmit={updatePasswordMe}
+          >
+            <div className="update-icon-box d-flex align-items-center justify-content-center rounded-circle">
+              <div className="update-icon rounded-circle">
+                <FaKey />
+              </div>
             </div>
-            <div className="box-input">
-              <InputPass
-                value={newPassword}
-                onChange={changNewPass}
-                placeholder="New password..."
-              />
-            </div>
-            <div className="box-input">
-              <InputPass
-                value={confirmPassword}
-                onChange={changeConfirmPass}
-                placeholder="Confirm password..."
-              />
-            </div>
+            <p className="title">Change Password</p>
+            <p className="text-center title-content">
+              Enter your new password below, we 're <br />
+              just being extra safe
+            </p>
+
+            <InputPass
+              title="Old password"
+              id="old-password"
+              value={oldPassword}
+              onChange={changeOldPass}
+              placeholder="Old password..."
+            />
+            <InputPass
+              title="New password"
+              id="new-password"
+              value={newPassword}
+              onChange={changNewPass}
+              placeholder="New password..."
+            />
+            <InputPass
+              title="Confirm password"
+              id="confirm-password"
+              value={confirmPassword}
+              onChange={changeConfirmPass}
+              placeholder="Confirm password..."
+            />
             <input
-              className="edit_profile_submit"
+              className="btn btn-sm btn-primary btn-submit"
               type="submit"
               value="Submit"
             />
